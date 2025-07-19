b1c518f6062cbefd54d0d9790479b2634d66f254
Ricardo Escaran <riczescaran@gmail.com>
2025-07-20 04:43:36 +0800

    Updated key mappings for optimal usage

--- keymap.c
+++ keymap.c
@@ -49,24 +49,24 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x6_3_ex2(
-        KC_ESC,           KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,      KC_LEFT,          KC_UP,   KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_TAB,           KC_A,    KC_S,    KC_D,    KC_F,     KC_G,      KC_RGHT,          KC_DOWN, KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_ESC,           KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,      KC_NO,            KC_NO,   KC_Y,      KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_TAB,           KC_A,    KC_S,    KC_D,    KC_F,     KC_G,      KC_NO,            KC_NO,   KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         TD(TD_LSFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                                 KC_N,      KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
                                                      KC_LCTL,  KC_LGUI,   KC_SPC,           KC_ENT,  MO(_NUMS), MO(_SYSTEM)
     ),
 
     [_NUMS] = LAYOUT_split_3x6_3_ex2(
-        KC_GRV,           KC_NO,   KC_PGUP, KC_NO,   KC_NO,    KC_NO,      KC_NO,            KC_NO,   KC_7,     KC_8,    KC_9,    KC_MINS, KC_EQL,  KC_BSPC,
-        KC_TAB,           KC_HOME, KC_PGDN, KC_END,  KC_NO,    KC_NO,      KC_NO,            KC_NO,   KC_4,     KC_5,    KC_6,    KC_LBRC, KC_RBRC, KC_BSLS,
-        TD(TD_LSFT_CAPS), KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,                                 KC_1,     KC_2,    KC_3,    KC_NO,   KC_NO,   KC_RALT,
+        KC_GRV,           KC_NO,   KC_UP,   KC_NO,   KC_NO,    KC_NO,      KC_NO,            KC_NO,   KC_7,     KC_8,    KC_9,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,           KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,    KC_NO,      KC_NO,            KC_NO,   KC_4,     KC_5,    KC_6,    KC_LBRC, KC_RBRC, KC_NO,
+        TD(TD_LSFT_CAPS), KC_PGUP, KC_PGDN, KC_HOME, KC_END,    KC_NO,                                KC_1,     KC_2,    KC_3,    KC_NO,   KC_BSLS, KC_NO,
                                                      KC_LCTL,  KC_LGUI,    KC_SPC,           KC_ENT,  KC_NO,    KC_0
     ),
 
     [_SYSTEM] = LAYOUT_split_3x6_3_ex2(
-        KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MUTE,    KC_MFFD,          KC_BRIU, RGB_TOG,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_VOLU,    KC_MRWD,          KC_BRID, RGB_VAI,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_VOLD,                               RGB_VAD,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                                     KC_NO,   KC_NO,      QK_BOOT,          QK_BOOT, KC_NO,    KC_NO
+        KC_NO,            KC_NO,   KC_VOLU, KC_NO,   RGB_TOG, KC_NO,      KC_NO,            KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,            KC_MRWD, KC_VOLD, KC_MFFD, RGB_VAI, KC_BRIU,    KC_NO,            KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,            KC_NO,   KC_MUTE, KC_NO,   RGB_VAD, KC_BRID,                               KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                                     KC_NO,   KC_NO,      KC_NO,            QK_BOOT, KC_NO,    KC_NO
     )
 };
 
